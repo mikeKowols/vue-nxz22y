a87a46cf0a91cc5874f8dde892921b8a7434273b
mikeKowols <mikekowols@gmail.com>
2020-10-15 12:03:12 -0500

    3 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img alt="Vue logo" src="https://vuejs.org/images/logo.png">
+		<HelloWorld msg="Mike Kowols" greeting="Greetings Earthlings!" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
@@ -17,12 +17,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-const { createApp } = require('vue');
+const { createApp } = require("vue");
 import App from "./App.vue";
 
 createApp(App).mount("#app");
+
+
